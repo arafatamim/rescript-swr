c2528dbcebab2efdf9b30d736af3816cfa4e839e
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2022-11-27 02:00:34 +0600

    update prepublish script

--- prepublish.sh
+++ prepublish.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env sh
 
-NPM_VERSION=$(jq ".version" package.json)
-BS_VERSION=$(jq ".version" bsconfig.json)
+NPM_VERSION=$(jq -r ".version" package.json)
+BS_VERSION=$(jq -r ".version" bsconfig.json)
+
+SWR_VERSION=$(jq ".dependencies.swr" package.json)
 
 if [ "$NPM_VERSION" != "$BS_VERSION" ]; then
   echo "Versions do not match. Exiting..."
+  exit 1
+fi
+
+read -p "Is SWR version ${SWR_VERSION} corresponding to package \
+version ${NPM_VERSION} correct? (y/n): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Aborted publishing process."
   exit 1
 fi
